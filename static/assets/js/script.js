47405ee19e53afd5f5a9fb49d9ed94ec9aec99d3
Sabuhi0 <sabuhiq0@gmail.com>
2022-02-11 20:36:17 +0400

    Add color switcher

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -1,3 +1,17 @@
+document.querySelector(".switcher-btn").onclick = () => {
+    document.querySelector(".color-switcher").classList.toggle('active');
+}
+
+let themeButtons = document.querySelectorAll('.theme-buttons');
+
+themeButtons.forEach(color => {
+    color.addEventListener('click', () => {
+        let dataColor = color.getAttribute('data-color');
+        document.querySelector(':root').style.setProperty('--color-mountain-meadow', dataColor)
+    })
+})
+
+
 // const clickNav = document.querySelector('.header-navbar-menu li a');
 
 // clickNav.onclick = function() {
