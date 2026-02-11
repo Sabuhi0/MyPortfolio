f299e55d7bc79defb6b92a58a33db1a48bdc1bb0
Sabuhi0 <sabuhiq0@gmail.com>
2026-02-11 15:56:01 +0400

    Fix PORT configuration for Railway deployment

--- run.py
+++ run.py
@@ -34,6 +34,6 @@ from app.routes import *
 #admin routes
 from admin.routes import *
 
-if __name__=='__main__':
-    # db.create_all()
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
